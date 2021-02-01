48e2fb631e6c32e8e3b2fca11b11e276b0879011
Aidan Magnus Powell <skitkined@gmail.com>
2021-01-31 23:18:11 -0700

    Made Plane fly

---
+++ Plane Challenge/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float rotationSpeed = 5.0f;
+    public float horizontalInput;
+    public float verticalInput;
+    
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * 1);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right, rotationSpeed * verticalInput * Time.deltaTime);
+        
+        
+    
+    }
+}
---
+++ Plane Challenge/Assets/propeller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class propeller : MonoBehaviour
+{
+    public float rotationSpeed = 1e+07f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          transform.Rotate(Vector3.forward, rotationSpeed * 1 * Time.deltaTime);
+    }
+}
