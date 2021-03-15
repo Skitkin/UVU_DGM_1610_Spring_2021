3d16c485e165f1e09ee12ad1056d44d5aec49151
Skitkin <77356868+Skitkin@users.noreply.github.com>
2021-03-15 17:49:28 -0600

    Update PlayerController.cs
    Deleted spaces

--- Proto 3/Assets/Scripts/PlayerController.cs
+++ Proto 3/Assets/Scripts/PlayerController.cs
@@ -12,14 +12,12 @@ public class PlayerController : MonoBehaviour
 
     private Animator playerAnim;
 
-
     public ParticleSystem dirtParticles;
     public ParticleSystem explosionParticles;
 
     public AudioClip jumpSound;
     public AudioClip crashSound;
     private AudioSource playerAudio;
-
 
     // Start is called before the first frame update
     void Start()
@@ -28,10 +26,7 @@ public class PlayerController : MonoBehaviour
         Physics.gravity *= gravityMod; 
 
         playerAnim = GetComponent<Animator>();
-
         playerAudio = GetComponent<AudioSource>();
-
-
     } 
         
 
@@ -44,11 +39,8 @@ public class PlayerController : MonoBehaviour
             isOnGround = false;
 
             playerAnim.SetTrigger("Jump_trig");
-
             dirtParticles.Stop();
             playerAudio.PlayOneShot(jumpSound, 1.0f );
-
-
             
         }
     }
@@ -71,7 +63,6 @@ public class PlayerController : MonoBehaviour
             Debug.Log("GAME OVER!!!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
-
             explosionParticles.Play();
             dirtParticles.Stop();
             playerAudio.PlayOneShot(crashSound, 1.0f );
