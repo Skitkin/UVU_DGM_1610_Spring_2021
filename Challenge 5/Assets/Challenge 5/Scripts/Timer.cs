aaaf19d7c946c6f8de1535d5cc1a218b135bd86e
Aidan Magnus Powell <skitkined@gmail.com>
2021-04-25 20:26:01 -0600

    finished Challenge 5

---
+++ Challenge 5/Assets/Challenge 5/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining = 60;
+    public bool timerIsRunning = false;
+    public TextMeshProUGUI TimerText;
+
+  
+    void Update()
+    {
+       TimerText.SetText("Time" + Mathf.RoundToInt(timeRemaining));
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+}
